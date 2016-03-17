26d5ed0be455c4263d18f162eb27d5897d889d9a
Paco Rampas <pacorampas@gmail.com>
2016-03-17 19:54:05 +0100

    first

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/hello/{name}', function($name) use($app) {
+  return $app['twig']->render('hello.twig', array(
+    'name' => $name,
+  ));
+  return 'Hello '.$app->escape($name);
+});
+
+$app->run();
